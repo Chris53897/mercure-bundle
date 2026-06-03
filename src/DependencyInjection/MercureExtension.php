17281afa6b628b728d00e1675b622bd6bda65956
chris <chris@sky-scripts.de>
2026-06-03 11:08:30 +0200

    feat: fix Extension deprecation since symfony 8.1

--- src/DependencyInjection/MercureExtension.php
+++ src/DependencyInjection/MercureExtension.php
@@ -21,7 +21,7 @@ use Symfony\Component\DependencyInjection\Compiler\AliasDeprecatedPublicServices
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\DependencyInjection\Definition;
-use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension:
 use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\Mercure\Authorization;
 use Symfony\Component\Mercure\Debug\TraceableHub;
